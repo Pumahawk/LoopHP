960b3fa5b5ce76aa53227688fdab5697861f8480
Lorenzo Gandino <lorenzoga94@gmail.com>
2018-10-04 16:46:28 +0200

    COnfigurato PHPUnit, Completato RouterConfiguration
    Creati test per router configuration

--- src/Config/Router/RouterConfiguration.php
+++ src/Config/Router/RouterConfiguration.php
@@ -6,64 +6,38 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\ArrayNode;
-use Symfony\Component\Config\Definition\ScalarNode;
+use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
 use Symfony\Component\Config\Definition\Processor;
 
 class RouterConfiguration  implements ConfigurationInterface {
-  protected $configuration;
-
-  public function getConfiguration() : array {
-    return $this -> configuration;
-  }
-  public function setConfiguration(array $configuration) {
-    $processor = new Processor();
-    $this -> configuration = $processor->processConfiguration(
-        $this,
-        $configuration
-    );
-  }
-
-  public function __construct(array $configuration) {
-    //$this -> setConfiguration($configuration);
-  }
 
   public function getConfigTreeBuilder() {
     $treeBuilder = new TreeBuilder();
 
-    $arrayNode = $treeBuilder -> root('address');
-    $arrayNode
-    -> children()
-      -> scalarNode('pattern') -> end()
-      -> arrayNode('data')
+    $patternNode = new ScalarNodeDefinition('pattern');
+    $dataNode = new ArrayNodeDefinition('data');
+    $dataNode -> children()
+      -> scalarNode('controller');
+    $addressNode = new ArrayNodeDefinition('address');
+    $addressNode
+      -> arrayPrototype()
+        -> append($patternNode)
+        -> append($dataNode)
         -> children()
-          -> scalarNode('controller') -> end()
-        -> end()
-      -> end()
-      -> variableNode('address')
-        -> validate()
-          -> always(function($data) use ($arrayNode){
-            $node = $arrayNode -> getNode();
-            var_dump($data);
-            $data = $node -> normalize($data);
-            $data = $node -> finalize($data);
-          })
-        -> end()
-      -> end()
-    -> end();
-    
-    return $treeBuilder;
-  }
-  public function validateRouter(ArrayNodeDefinition &$node) {
-    $node
-      -> children()
-        -> scalarNode('val1') -> end()
-        -> scalarNode('val2') -> end()
-      -> end();
-    $routerNode = $node -> children() -> arrayNode('router');
-    $routerNode -> validate() -> always(function($val) use ($routerNode) {
-      $routerNode -> finalize($val);
-      return $val;
-    });
+          -> variableNode('address')
+            -> validate() -> always(function($var) use ($addressNode){
+              $node = $addressNode -> getNode();
+              $node -> normalize($var);
+              $node -> finalize($var);
+              return $var;
+            })
+          -> end();
 
+
+    $treeBuilder -> root('router') -> arrayPrototype()
+    -> append($patternNode)
+    -> append($dataNode)
+    -> append($addressNode);
+    return $treeBuilder;
   }
 }
---
+++ src/Test/Config/Router/RouterConfigurationTest.php
@@ -0,0 +1,123 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use LoopHP\Config\Router\RouterConfiguration;
+use Symfony\Component\Config\Definition\Processor;
+
+final class RouterConfigurationTest extends TestCase
+{
+    /**
+    * @dataProvider routerValidConfigurationList
+    * @doesNotPerformAssertions
+    */
+    public function testProcessValidConfiguration($configuration): void
+    {
+        $routerConf = new RouterConfiguration();
+        $pro = new Processor();
+        $pro -> processConfiguration($routerConf, $configuration);
+    }
+
+    public function routerValidConfigurationList() {
+      $configurationList = array();
+
+      //Configuration 0
+      $configurationList[] = [[
+        'router' => [
+          [
+            'pattern' => 'pattern',
+            'data' => [
+              'controller' => 'controller'
+            ]
+          ]
+        ]
+      ]];
+
+      //Configuration 1
+      $configurationList[] = [[
+        'router' => [
+          [
+            'pattern' => 'pattern',
+            'data' => [
+              'controller' => 'controller'
+            ]
+          ],
+          [
+            'pattern' => 'pattern',
+            'data' => [
+              'controller' => 'controller'
+            ]
+          ]
+        ]
+      ]];
+
+      //Configuration 2
+      $configurationList[] = [[
+        'router' => [
+          [
+            'pattern' => 'pattern',
+            'address' => [
+              [
+                'pattern' => 'pattern',
+                'data' => [
+                  'controller' => 'controller'
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]];
+
+      //Configuration 3
+      $configurationList[] = [[
+        'router' => [
+          [
+            'pattern' => 'pattern',
+            'address' => [
+              [
+                'pattern' => 'pattern',
+                'address' => [
+                  [
+                    'pattern' => 'pattern',
+                    'data' => [
+                      'controller' => 'controller'
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]];
+
+      //Configuration 3
+      $configurationList[] = [[
+        'router' => [
+          [
+            'pattern' => 'pattern',
+            'address' => [
+              [
+                'pattern' => 'pattern',
+                'address' => [
+                  [
+                    'pattern' => 'pattern',
+                    'data' => [
+                      'controller' => 'controller'
+                    ]
+                  ],
+                  [
+                    'pattern' => 'pattern',
+                    'data' => [
+                      'controller' => 'controller'
+                    ]
+                  ]
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]];
+
+      return $configurationList;
+    }
+}
