f91c209b9ba9678770fce1bb9852e7f1a0f732a5
Lorenzo Gandino <lorenzoga94@gmail.com>
2018-10-10 12:28:33 +0200

    Creazione test per la relazione tra app e controller

---
+++ resources/test/controller/ControllerRelation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Controller;
+use LoopHP\Controller\BaseController;
+
+class ControllerRelation extends BaseController {
+  public function hallo() {
+    echo 'Hallo world!';
+  }
+}
---
+++ src/Test/Relation/AppControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace LoopHP\Test\Relation;
+
+use PHPUnit\Framework\TestCase;
+use LoopHP\App;
+use LoopHP\ControllerData;
+use LoopHP\AppConfiguration;
+use LoopHP\Matchable;
+
+class AppControllerTest extends TestCase {
+  public function getConfiguration() {
+    $conf = new AppConfiguration();
+    $conf
+      -> setTemplate(__DIR__.'/../../../resources/test/view')
+      -> addConfigurationPath(__DIR__.'/../../../resources/test/config')
+      -> setComposer(require(__DIR__.'/../../../vendor/autoload.php'))
+      -> addApi('Controller\\', __DIR__.'/../../../resources/test/controller');
+    return $conf;
+  }
+  public function getMachable(string $method, array $data = array()) {
+    return new class($method, $data) implements Matchable {
+      protected $data;
+      public function __construct(string $method, array $data = array()) {
+        $this -> data = new ControllerData('Controller\\ControllerRelation', $method, $data);
+      }
+      public function match() : ControllerData {
+        return $this -> data;
+      }
+    };
+  }
+  public function testExecuteController() {
+    $this -> expectOutputString('Hallo world!');
+    $match = $this -> getMachable('hallo');
+    $appc = $this -> getConfiguration();
+
+    $app = new App($appc, $match);
+    $app -> setApiLoader();
+    $app -> start();
+  }
+}
